91440c283a298c13831f04f305a508e34b214741
Manar0887 <102428833+Manar0887@users.noreply.github.com>
2022-04-26 19:16:16 +0200

    Project Initial code

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//شغل java لم يطبق
+
+//select landing page elemwnt
+let landingpage = document.querySelector(".landing-page");
+//get arrey of imags
+let imagsArray = ["image1", "image2", "image3", "image4"];
+
+setInterval(() => {
+  //GET Random Number
+  let randomNuber = Math.floor(Math.random() * imagsArray.length);
+  console.log(randomNuber);
+  
+  //change background  image url
+  landingpage.style.backgroundImage =
+    `url("../imgs/backgrounds/${imagsArray[randomNuber]}.jpeg")`;
+}, 5000);
