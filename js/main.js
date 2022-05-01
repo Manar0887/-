bcaf2b85937730664114545e79ada42b6abcdbd0
Manar0887 <102428833+Manar0887@users.noreply.github.com>
2022-05-01 04:52:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-//شغل java لم يطبق
-
-//select landing page elemwnt
+//select page-top section element
 let landingpage = document.querySelector(".page-top");
 //get arrey of imags
 let imagsArray = ["image1", "image2", "image3", "image4"];
@@ -11,5 +9,5 @@ setInterval(() => {
   
   //change background  image url
   landingpage.style.backgroundImage =
-    `url("../imgs/backgrounds/${imagsArray[randomNuber]}.jpeg")`;
+    `url("./imgs/backgrounds/${imagsArray[randomNuber]}.jpeg")`;
 }, 5000);
